f8f4edb28dac19b139f4541c66b0dbe510c36e11
Gaurav Panchal <gaurav.rockersinfo@gmail.com>
2018-11-13 14:56:47 +0530

    iOS Commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput,Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,14 +18,52 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {email: '', password: ''}
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Rockers Technology!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+          <View style={styles.inputWrapper}>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Your Email"
+                  onChangeText={(email) => this.setState({email})}
+                  value={this.state.email}
+                  underlineColorAndroid='rgba(0,0,0,0)'
+                  autoCapitalize='none'
+              />
+          </View>
+          <View style={styles.inputWrapper}>
+              <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  autoCapitalize='none'
+                  onChangeText={(password) => this.setState({password})}
+                  value={this.state.password}
+                  underlineColorAndroid='rgba(0,0,0,0)'
+                  secureTextEntry={true}
+              />
+          </View>
+          <View style={{marginTop: 20}} />
+          <Button
+              style={styles.btnContainer}
+              onPress={() => {this.login()}}
+              value="GO"
+          />
+          {/* <Link to="/signup">
+              <Text>Register</Text>
+          </Link> */}
       </View>
-    );
+  );
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>Welcome to React123 Native!</Text>
+    //     <Text style={styles.instructions}>To get started, edit App.js</Text>
+    //     <Text style={styles.instructions}>{instructions}</Text>
+    //   </View>
+    // );
   }
 }
 
